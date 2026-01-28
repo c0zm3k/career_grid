ebe845d32a36d9bf496248e00d617f8615517263
KASHIP KRISHNA P K <222924894+c0zm3k@users.noreply.github.com>
2026-01-29 01:12:01 +0530

    chore: update superadmin credentials and fix script import path

--- scripts/reset_db.py
+++ scripts/reset_db.py
@@ -1,4 +1,7 @@
 import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from app import db, create_app
 from app.models import User
 from werkzeug.security import generate_password_hash
